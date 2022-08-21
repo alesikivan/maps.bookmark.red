c9c82667034dcc8575922a00c5a482554c575a97
alesikivan <obcofficiall@gmail.com>
2022-08-21 16:48:09 +0300

    fix (app): change config files

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   apps : [{
-    script: './core/coloring.js',
+    script: './core/maps.js',
     env : {
       'PORT': 80,
       'NODE_ENV' : 'production'
@@ -12,8 +12,8 @@ module.exports = {
       user : 'root',
       host : ['45.93.136.98'],
       ref  : 'origin/main',
-      repo : 'https://github.com/alesikivan/coloring.bookmark.red',
-      path : '/root/apps/bookmark-coloring',
+      repo : 'https://github.com/alesikivan/maps.bookmark.red',
+      path : '/root/apps/kepler-map',
       'pre-deploy-local': '',
       'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production',
       'pre-setup': ''
